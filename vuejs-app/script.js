987fdbaeab47ccdece2492da18133b9124ef5349
Salma-Reda-Moltaqa <salma-reda@moltaqa.net>
2024-07-29 11:08:54 +0300

    print data using vue

--- vuejs-app/script.js
+++ vuejs-app/script.js
@@ -1,23 +1,6 @@
-const { createApp } = Vue;
-
-createApp({
-    data() {
-        return {
-            name: "Salma",
-            age: 22,
-            link: "test.com",
-            number: 0
-        };
-    },
-    methods: {
-        sayHi(time, name) {
-            return "Good " + time + ', ' + name;
-        },
-        sayTest: function() {
-            return "Loma";
-        },
-        increment() {
-            this.number++;
-        },
+new Vue({
+    el: "#app-root",
+    data: {
+        name: "salma"
     }
-}).mount("#app-root");
+})
